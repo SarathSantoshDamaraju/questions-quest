f660ef1b01589a1525b9e32606be1c1faad18171
SarathSantoshDamaraju <krishna@gohighlevel.com>
2023-03-18 23:27:31 +0530

    chore: logo

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,19 @@
-import { useEffect, useRef } from 'react'
-import Link from 'next/link'
-import BLOG from '@/blog.config'
-import { useLocale } from '@/lib/locale'
+import { useEffect, useRef } from "react";
+
+import BLOG from "@/blog.config";
+import Link from "next/link";
+import { useLocale } from "@/lib/locale";
 
 const NavBar = () => {
-  const locale = useLocale()
+  const locale = useLocale();
   const links = [
-    { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
-    { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
-    { id: 2, name: locale.NAV.RSS, to: '/feed', show: true },
-    { id: 3, name: locale.NAV.SEARCH, to: '/search', show: true }
-  ]
+    { id: 0, name: locale.NAV.SEARCH, to: "/search", show: true },
+  ];
   return (
     <div className="flex-shrink-0">
       <ul className="flex flex-row">
         {links.map(
-          link =>
+          (link) =>
             link.show && (
               <li
                 key={link.id}
@@ -27,39 +25,39 @@ const NavBar = () => {
         )}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 const Header = ({ navBarTitle, fullWidth }) => {
-  const useSticky = !BLOG.autoCollapsedNavBar
-  const navRef = useRef(null)
-  const sentinelRef = useRef([])
+  const useSticky = !BLOG.autoCollapsedNavBar;
+  const navRef = useRef(null);
+  const sentinelRef = useRef([]);
   const handler = ([entry]) => {
     if (navRef && navRef.current && useSticky) {
       if (!entry.isIntersecting && entry !== undefined) {
-        navRef.current?.classList.add('sticky-nav-full')
+        navRef.current?.classList.add("sticky-nav-full");
       } else {
-        navRef.current?.classList.remove('sticky-nav-full')
+        navRef.current?.classList.remove("sticky-nav-full");
       }
     } else {
-      navRef.current?.classList.add('remove-sticky')
+      navRef.current?.classList.add("remove-sticky");
     }
-  }
+  };
   useEffect(() => {
-    const obvserver = new window.IntersectionObserver(handler)
-    obvserver.observe(sentinelRef.current)
+    const obvserver = new window.IntersectionObserver(handler);
+    obvserver.observe(sentinelRef.current);
     // Don't touch this, I have no idea how it works XD
     // return () => {
     //   if (sentinalRef.current) obvserver.unobserve(sentinalRef.current)
     // }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [sentinelRef])
+  }, [sentinelRef]);
   return (
     <>
       <div className="observer-element h-4 md:h-12" ref={sentinelRef}></div>
       <div
         className={`sticky-nav m-auto w-full h-6 flex flex-row justify-between items-center mb-2 md:mb-12 py-8 bg-opacity-60 ${
-          !fullWidth ? 'max-w-3xl px-4' : 'px-4 md:px-24'
+          !fullWidth ? "max-w-3xl px-4" : "px-4 md:px-24"
         }`}
         id="sticky-nav"
         ref={navRef}
@@ -67,33 +65,12 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <div className="flex items-center">
           <Link href="/" aria-label={BLOG.title}>
             <div className="h-6">
-              <svg
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect
-                  width="24"
-                  height="24"
-                  className="fill-current text-black dark:text-white"
-                />
-                <rect width="24" height="24" fill="url(#paint0_radial)" />
-                <defs>
-                  <radialGradient
-                    id="paint0_radial"
-                    cx="0"
-                    cy="0"
-                    r="1"
-                    gradientUnits="userSpaceOnUse"
-                    gradientTransform="rotate(45) scale(39.598)"
-                  >
-                    <stop stopColor="#CFCFCF" stopOpacity="0.6" />
-                    <stop offset="1" stopColor="#E9E9E9" stopOpacity="0" />
-                  </radialGradient>
-                </defs>
-              </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-moped" width="48" height="48" viewBox="0 0 24 24" stroke-width="1.5" stroke="#009988" fill="none" stroke-linecap="round" stroke-linejoin="round">
+  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+  <circle cx="18" cy="17" r="2" />
+  <path d="M5 16v1a2 2 0 0 0 4 0v-5h-3a3 3 0 0 0 -3 3v1h10a6 6 0 0 1 5 -4v-5a2 2 0 0 0 -2 -2h-1" />
+  <line x1="6" y1="9" x2="9" y2="9" />
+</svg>
             </div>
           </Link>
           {navBarTitle ? (
@@ -102,7 +79,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
             </p>
           ) : (
             <p className="ml-2 font-medium text-day dark:text-night header-name">
-              {BLOG.title},{' '}
+              {BLOG.title},{" "}
               <span className="font-normal">{BLOG.description}</span>
             </p>
           )}
@@ -110,7 +87,7 @@ const Header = ({ navBarTitle, fullWidth }) => {
         <NavBar />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
